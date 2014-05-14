7ee4034bbc211d65037fd55bbbe730d2c1fb5cfe
Kakysha <ezhikvdele@gmail.com>
2014-05-15 01:49:40 +0400

    Initial Commit

---
+++ LockFree/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LockFree
+//
+//  Created by Drunk on 15.05.14.
+//  Copyright (c) 2014 home. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
